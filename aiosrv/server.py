76b22100ffcd6d1309c6fbb60d499b4f1a2b2e0f
Tao Li <tao.9900@gmail.com>
2016-07-03 13:07:12 -0700

    aiohttp hello world

--- aiosrv/server.py
+++ aiosrv/server.py
@@ -1,6 +1,29 @@
+import argparse
+from aiohttp import web
+from aiosrv.handlers.hello_world import hello
+
+ROUTES = [["GET", "/", hello]]
+
+
+def init():
+    app = web.Application()
+    for route in ROUTES:
+        app.router.add_route(*route)
+    return app
+
 
 def main():
-    print("Hello World")
+    p = argparse.ArgumentParser()
+    p.add_argument("--host",
+                   dest="host",
+                   default="0.0.0.0",
+                   required=False)
+    p.add_argument("--port",
+                   dest="port",
+                   default=8080,
+                   required=False)
+    args = p.parse_args()
+    web.run_app(init(), host=args.host, port=args.port)
 
 if __name__ == "__main__":
     main()
